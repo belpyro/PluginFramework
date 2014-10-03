9468ad8b82e630823f02eb76dfe953f5d473558d
Alexander Shaduro <alexander_shaduro@epam.com>
2014-10-03 14:11:18 +0300

    + added sources

---
+++ MonoBehaviorWindowExtended.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PluginFramework
+{
+    public abstract class MonoBehaviorWindowExtended : MonoBehaviourWindow
+    {
+        protected virtual void Close()
+        {
+            if (OnClosed != null)
+            {
+                OnClosed(this, EventArgs.Empty);
+            }
+        }
+
+        public event EventHandler OnClosed;
+    }
+}
